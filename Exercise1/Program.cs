7d67e66e3c718b660712cd6941f88969bb8b55cf
PrashastVats1 <prashastvats@gmail.com>
2023-07-13 09:39:45 +0530

    exercise commit

---
+++ Exercise1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pre, final;
+            Console.WriteLine("Enter the Pre Marks");
+            pre = int.Parse(Console.ReadLine());
+            if (pre > 60)
+            {
+                Console.WriteLine("Enter the Final Marks");
+                final = int.Parse(Console.ReadLine());
+                if (final > 55)
+                {
+                    Console.WriteLine("Selected");
+                }
+                else
+                {
+                    Console.WriteLine("Failed in Final");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Failed in Pre");
+            }
+            Console.ReadKey();
+        }
+    }
+}
