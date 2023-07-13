f4313382f00cecc084c471783adcb65a7f12f4ae
PrashastVats1 <prashastvats@gmail.com>
2023-07-13 09:33:05 +0530

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        enum Seasons {
+            winter,
+            summer,
+            rainy
+        }
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.WriteLine("Enter first number");
+            num1=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            num2=int.Parse(Console.ReadLine());
+            var result = num1 > num2 ? "First number is greater" : "Second number is greater";
+            Console.WriteLine(result);
+
+            Console.WriteLine("Current Season is: " + Seasons.rainy);
+            Console.WriteLine("Season Number is: " + (int)Seasons.rainy);
+            Console.ReadKey();
+        }
+    }
+}
